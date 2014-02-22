5f5f8817dca7a8c0365bda81d4091b9949339c5e
monai <monai@cure.lt>
2014-02-22 11:41:06 +0200

    refactoring

--- cli.js
+++ cli.js
@@ -1,19 +1,41 @@
 #!/usr/bin/env node
 
+var fs = require('fs');
+var path = require('path');
 var optimist = require('optimist');
 var async = require('async');
 var slugify = require('./index');
 
 var argv = optimist
 .usage('Usage: $0 [OPTIONS] FILE')
-.describe('e', 'normalize file extension')
+.describe('e', 'lowercase file extension')
 .demand(1)
 .argv;
+
+var dirname, extname, basename, join;
+var dir, ext, base, toFile, lowerExt;
+
+dirname = path.dirname;
+extname = path.extname;
+basename = path.basename;
+join = path.join;
+lowerExt = !! argv.e;
 
 async.eachSeries(argv._, _slugify, function (error) {
     if (error) throw error;
 });
 
-function _slugify(filename, callback) {
-    slugify(filename, !! argv.e, callback);
+function _slugify(file, callback) {
+    dir = dirname(file);
+    ext = extname(file);
+    base = basename(file, ext);
+    base = slugify(base);
+    
+    if (lowerExt) {
+        ext = ext.toLowerCase();
+    }
+    
+    toFile = path.join(dir, base + ext);
+    
+    fs.rename(file, toFile, callback);
 }
--- index.js
+++ index.js
@@ -1,33 +1,8 @@
-var fs = require('fs');
-var path = require('path');
 var unidecode = require('unidecode');
 
 module.exports = slugify;
 
-var dirname, extname, basename, join;
-var dir, ext, base, toFile;
-
-dirname = path.dirname;
-extname = path.extname;
-basename = path.basename;
-join = path.join;
-
-function slugify(file, normalizeExt, callback) {
-    dir = dirname(file);
-    ext = extname(file);
-    base = basename(file, ext);
-    base = _slugify(base);
-    
-    if (normalizeExt) {
-        ext = ext.toLowerCase();
-    }
-    
-    toFile = path.join(dir, base + ext);
-    
-    fs.rename(file, toFile, callback);
-}
-
-function _slugify(str) {
+function slugify(str) {
     var out, re, i, l, stri;
     
     re = /[\x00-\x2f\x3a-\x40\x5b-\x60\x7b-\x7f]/
